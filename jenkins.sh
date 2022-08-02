f0982ba5ebbe80316bd208832508a5286c5d223b
iampiyushsaraswat <iampiyushsaraswat@gmail.com>
2022-08-02 23:11:22 +0530

    change txt to .sh for executable

--- jenkins.sh
+++ jenkins.sh
@@ -1,12 +1,12 @@
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
-sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt install openjdk-8-jdk
-sudo apt -y update
-sudo apt -y install jenkins
-sudo systemctl start jenkins.service
-sudo ufw allow 8080
-sudo ufw allow OpenSSH
-sudo ufw enable
+sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt install openjdk-8-jdk
+apt -y update
+apt -y install jenkins
+systemctl start jenkins.service
+ufw allow 8080
+ufw allow OpenSSH
+ufw enable
 
 
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
